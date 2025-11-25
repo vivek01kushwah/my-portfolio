a975d2e4aa2a9db4b82c0ee6ddf20937b96acfd8
Vivek Kushwah <vivek0709kushwah@gmail.com>
2025-11-26 01:15:25 +0530

    Fix basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,9 @@ const nextConfig = {
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' }
     ]
+  },
+  experimental: {
+    typedRoutes: false
   }
 };
 
